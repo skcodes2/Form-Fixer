f505cb5f4451247cfcd3a9130b71c1d19e51fcb2
Dasol Lim <dlim67@uwo.ca>
2025-01-23 17:33:28 -0500

    Update exercise name and url

--- app/WorkoutPlan/ExcerciseData.ts
+++ app/WorkoutPlan/ExcerciseData.ts
@@ -5,25 +5,25 @@ const ExerciseData = {
             "name": "Bench Press",
             "description": "Lay on a bench and push the barbell up and down.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/benchPress.jpg"
         },
         {
             "name": "Incline Bench Press",
             "description": "Lay on an incline bench and push the barbell upward to target the upper chest.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/inclineBenchPress.jpg"
         },
         {
             "name": "Chest Fly",
             "description": "Use dumbbells or a machine to perform a wide arc motion, bringing your hands together to target the chest.",
             "timePerRep": 3,
-            "url": ""
+            "url": "assets/images/workout/chestFly.jpg"
         },
         {
             "name": "Push-Ups",
             "description": "Perform a bodyweight exercise by pushing your body up from the ground, targeting the chest and triceps.",
             "timePerRep": 1.5,
-            "url": ""
+            "url": "assets/images/workout/pushUp.jpeg"
         },
     ],
 
@@ -32,31 +32,31 @@ const ExerciseData = {
             "name": "Pull-Ups",
             "description": "Hang from a bar and pull yourself up until your chin is above the bar.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/pullUp.png"
         },
         {
             "name": "Deadlift",
             "description": "Lift a barbell from the ground to your hips, focusing on the back and legs.",
             "timePerRep": 3,
-            "url": ""
+            "url": "assets/images/workout/deadlift.jpg"
         },
         {
             "name": "Bent-Over Rows",
             "description": "Hold a barbell or dumbbells and row them to your torso while bent over.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/bentOverRow.jpg"
         },
         {
             "name": "Lat Pulldown",
             "description": "Pull a cable bar down to your chest to target the latissimus dorsi.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/latPullDown.png"
         },
         {
             "name": "Rows",
             "description": "Use a T-bar row machine or barbell setup to pull the weight toward your torso.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/rows.png"
         }
     ],
 
@@ -65,31 +65,19 @@ const ExerciseData = {
             "name": "Squats",
             "description": "Lower your body into a sitting position and stand back up, targeting the legs and glutes.",
             "timePerRep": 2,
-            "url": ""
-        },
-        {
-            "name": "Lunges",
-            "description": "Step forward with one leg, lowering your body until your back knee nearly touches the floor.",
-            "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/squat.png"
         },
         {
             "name": "Leg Press",
             "description": "Push the weight away using your legs on a leg press machine.",
             "timePerRep": 2,
-            "url": ""
-        },
-        {
-            "name": "Romanian Deadlift",
-            "description": "Use a barbell or dumbbells to lower the weight along your legs, targeting hamstrings and glutes.",
-            "timePerRep": 3,
-            "url": ""
+            "url": "assets/images/workout/legPress.png"
         },
         {
             "name": "Calf Raises",
             "description": "Lift your heels off the ground to target the calves.",
             "timePerRep": 1.5,
-            "url": ""
+            "url": "assets/images/workout/calfRaise.png"
         }
     ],
 
@@ -98,64 +86,64 @@ const ExerciseData = {
             "name": "Bicep Curls",
             "description": "Curl dumbbells or a barbell to your chest to target the biceps.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/biceptCurl.png"
         },
         {
             "name": "Tricep Dips",
             "description": "Lower and raise your body using parallel bars to target the triceps.",
             "timePerRep": 2.5,
-            "url": ""
+            "url": "assets/images/workout/triceptDip.png"
+        },
+        {
+            "name": "Tricep Pull Down",
+            "description": "Lower and raise your body using parallel bars to target the triceps.",
+            "timePerRep": 2.5,
+            "url": "assets/images/workout/triceptPullDown.png"
         },
         {
             "name": "Hammer Curls",
             "description": "Curl dumbbells with a neutral grip to target the biceps and forearms.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/hammerCurl.png"
         },
         {
             "name": "Skull Crushers",
             "description": "Lie on a bench and lower a barbell or dumbbells to your forehead to target the triceps.",
             "timePerRep": 2.5,
-            "url": ""
+            "url": "assets/images/workout/skullCrusher.png"
         },
         {
             "name": "Preacher Curls",
             "description": "Use a preacher curl bench to isolate the biceps while curling a barbell or dumbbell.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/preacherCurl.png"
         }
     ],
 
     Shoulders: [
         {
-            "name": "Overhead Press",
-            "description": "Press a barbell or dumbbells overhead to target the shoulders.",
-            "timePerRep": 2,
-            "url": ""
-        },
-        {
             "name": "Lateral Raises",
             "description": "Raise dumbbells to your sides to target the lateral deltoids.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/lateralRaise.png"
         },
         {
             "name": "Front Raises",
             "description": "Raise dumbbells in front of you to target the anterior deltoids.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/frontRaise.png"
         },
         {
-            "name": "Arnold Press",
+            "name": "Shoulder Press",
             "description": "Rotate dumbbells as you press them overhead, targeting all parts of the deltoids.",
             "timePerRep": 2.5,
-            "url": ""
+            "url": "assets/images/workout/shoulderPress.png"
         },
         {
             "name": "Rear Delt Fly",
             "description": "Bend over and raise dumbbells outward to target the rear delts.",
             "timePerRep": 2,
-            "url": ""
+            "url": "assets/images/workout/rearDeltFly.png"
         }
     ],
 
